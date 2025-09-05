9c8d07d9a8e5b74a5475d27fc75947c781737960
dragonenderguy2 <shaihalapre@hotmail.com>
2025-09-05 13:15:01 -0500

    feat: initial commit for src/utils.py

---
+++ src/utils.py
@@ -0,0 +1,8 @@
+def check_open_standard(standard):
+    # Vérifie si la norme est conforme aux normes ouvertes
+    return f'{standard} est une norme ouverte.'
+
+
+def format_documentation(title, content):
+    # Formate la documentation pour l'affichage
+    return f'# {title}\n\n{content}'
